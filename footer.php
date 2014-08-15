39e73e6460d4ff9c9d9c9e3893e6dd15e91d3126
Edgar <edgar.serventek@gmail.com>
2014-08-14 19:18:31 -0500

    updated image paths

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer> Brooke Axtell All Rights Reserved
 	<div class="SocialIcons">
-		<a target="_blank" href="https://twitter.com/SurvivorHealing"><img src="Twitter.png" /></a>
-		<a target="_blank" href="https://www.facebook.com/brooke.e.axtell?fref=ts"><img src="Facebook.png" /></a>
+		<a target="_blank" href="https://twitter.com/SurvivorHealing"><img src="images/Twitter.png" /></a>
+		<a target="_blank" href="https://www.facebook.com/brooke.e.axtell?fref=ts"><img src="images/Facebook.png" /></a>
 	</div>	
 </footer>
