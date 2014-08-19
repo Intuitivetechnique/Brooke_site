d8fbb2bb8009c78fcfa1cf908e6c50064d9b0894
Intuitivetechnique <Intuitivetechnique@gmail.com>
2014-08-18 21:42:40 -0500

    adding old contact.php

--- contact.php
+++ contact.php
@@ -1,21 +1,72 @@
 <?php
-
-	$name=$_POST["name"];
-	
-	$from = 'From: test.com'; 
-    $to = 'edgar.serventek@gmail.com'; 
-    $subject = 'New Sign Up!';
-
-    $body = "From: $name ";
-	
-
-		
-    if (mail ($to, $subject, $body, $from)) { 
-        print '<p>Your message has been sent!</p>';
-    } else { 
-        error_log("something is wrong");
+if($_POST)
+{
+    $to_Email       = "intuitivetechnique@gmail.com"; //Replace with recipient email address
+    $subject        = 'Ah!! My email from Somebody out there...'; //Subject line for emails
+    
+    
+    //check if its an ajax request, exit if not
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+    
+        //exit script outputting json data
+        $output = json_encode(
+        array(
+            'type'=>'error', 
+            'text' => 'Request must come from Ajax'
+        ));
+        
+        die($output);
+    } 
+    
+    //check $_POST vars are set, exit if any missing
+    if(!isset($_POST["userName"]) || !isset($_POST["userEmail"]) || !isset($_POST["userPhone"]) || !isset($_POST["userMessage"]))
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Input fields are empty!'));
+        die($output);
     }
-	 
 
+    //Sanitize input data using PHP filter_var().
+    $user_Name        = filter_var($_POST["userName"], FILTER_SANITIZE_STRING);
+    $user_Email       = filter_var($_POST["userEmail"], FILTER_SANITIZE_EMAIL);
 
+    
+    //additional php validation
+    if(strlen($user_Name)<2) // If length is less than 4 it will throw an HTTP error.
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
+        die($output);
+    }
+    if(!filter_var($user_Email, FILTER_VALIDATE_EMAIL)) //email validation
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
+        die($output);
+    }
+
+    
+    //proceed with PHP email.
+
+    /*
+    Incase your host only allows emails from local domain, 
+    you should un-comment the first line below, and remove the second header line. 
+    Of-course you need to enter your own email address here, which exists in your cp.
+    */
+    //$headers = 'From: your-name@YOUR-DOMAIN.COM' . "\r\n" .
+    $headers = 'From: '.$user_Email.'' . "\r\n" . //remove this line if line above this is un-commented
+    'Reply-To: '.$user_Email.'' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    
+        // send mail
+    $sentMail = @mail($to_Email, $subject, $user_Message .'  -'.$user_Name, $headers);
+    
+    if(!$sentMail)
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
+        die($output);
+    }else{
+        $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$user_Name .' Thank you for your email'));
+        die($output);
+    }
+}
 ?>
+
+
